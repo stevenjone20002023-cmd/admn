1c90e86a66944ab17ce8b807443731c49425ee2c
stevenjone20002023-cmd <steven.jone.2000.2023@gmail.com>
2026-03-28 21:26:38 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.MY_STORE_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyB2_KZ9Gn1e533viEORuzxeoMnXHrJg1yI",
+    authDomain: "iuyhiuhy.firebaseapp.com",
+    projectId: "iuyhiuhy",
+    storageBucket: "iuyhiuhy.firebasestorage.app",
+    messagingSenderId: "737406385560",
+    appId: "1:737406385560:web:89ebc5763346810d86bfed"
+  },
+  security: {
+    adminCode: "1001"
+  }
+};
